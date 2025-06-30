b2b6b42a766b764ea505212d036fa081b69d8b6d
Kelemen Richard <kelemen.richard1@gmail.com>
2025-06-30 20:18:12 +0200

    update default_minecraft_path according to user OS

--- modrinth_updater/config.py
+++ modrinth_updater/config.py
@@ -1,14 +1,35 @@
-from dotenv import load_dotenv
 import os
+import platform
+from dotenv import load_dotenv
 
 load_dotenv(override=True)
-env_mc_path = os.getenv('DEFAULT_MC_FOLDER')
-if env_mc_path is not None:
-    default_minecraft_path = env_mc_path
-else:
-    appdata_path = os.getenv('APPDATA')
-    default_minecraft_path = os.path.join(appdata_path, '.minecraft')
+system = platform.system()
+
+# If your system is Windows, then the default path is C:\Users\%USERNAME%\AppData\Roaming\.minecraft
+if system == 'Windows':
+    env_mc_path = os.getenv('DEFAULT_WINDOWS_MC_FOLDER')
+    if env_mc_path is not None:
+        default_minecraft_path = env_mc_path
+    else:
+        appdata_path = os.getenv('APPDATA')
+        default_minecraft_path = os.path.join(appdata_path, '.minecraft')
+# If your system is macOS, then the default path is ~/Library/Application Support/minecraft
+elif system == 'Darwin':
+    env_mc_path = os.getenv('DEFAULT_MACOS_MC_FOLDER')
+    if env_mc_path is not None:
+        default_minecraft_path = env_mc_path
+    else:
+        default_minecraft_path = os.path.expanduser('~/Library/Application Support/minecraft')
+# If your system is Linux, then the default path is ~/.minecraft
+elif system == 'Linux':
+    env_mc_path = os.getenv('DEFAULT_LINUX_MC_FOLDER')
+    if env_mc_path is not None:
+        default_minecraft_path = env_mc_path
+    else:
+        default_minecraft_path = os.path.expanduser('~/.minecraft')
 
 env_run_mods_update = os.getenv('RUN_MODS_UPDATER')
 env_run_resourepacks_update = os.getenv('RUN_RESOUREPACKS_UPDATER')
 env_run_shaderpacks_update = os.getenv('RUN_SHADERPACKS_UPDATER')
+
+# End-of-file
